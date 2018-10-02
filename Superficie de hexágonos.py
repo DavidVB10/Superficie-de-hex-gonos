acf5923e08da32338d35c8f3d86912b4ca509b9a
DavidVB10 <dvillablanco@gmail.com>
2018-10-03 01:00:56 +0200

    Add files via upload

---
+++ Superficie de hexágonos.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+l=input("Introduzca la medida en cm de un lado del hexágono = ")
+a=input("Introduzca la medida en cm de la apotema del hexágono = ")
+print("La superficie del hexágono es de " + str(((l*6)*a)/2) + " cm2")
